ef983ec7e733dbc957759f8faf03178a445b99b5
murmu <murmurl912@outlook.com>
2019-09-12 09:39:14 +0800

    add opencv java lib

---
+++ app/src/main/java/com/example/mars/ui/camera/CameraPreview.java
@@ -0,0 +1,12 @@
+package com.example.mars.ui.camera;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class CameraPreview extends SurfaceView {
+
+    public CameraPreview(Context context) {
+        super(context);
+    }
+
+}
